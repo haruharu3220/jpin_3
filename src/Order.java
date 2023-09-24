a7106b153be3495c14899033ec75f85f6482e235
Hayato <hayato.314.nico@gmail.com>
2023-09-24 23:44:24 +0900

    getTotalメソッドの追加

--- src/Order.java
+++ src/Order.java
@@ -2,6 +2,21 @@ public class Order {
     private final Item item;
     private final Quantity quantity;
 
+    public Order(Item item, Quantity quantity) {
+        super();
+        if(item == null ) throw  new IllegalArgumentException();
+        if(quantity == null)    throw   new IllegalArgumentException();
 
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public Total getTtal(){
+        //悪い例　理由＝カプセル化違反
+        //int result = this.item.getPrice().getAmount() * this.quantity.getAmount();
+        //return new Total(result);
+        ItemPrice price = this.item.getPrice().multi(quantity);
+        return new Total(price.getAmount());
+    }
 
 }
