40bde968bb8b25016b7f8a4ce5ca28d2332e67bd
Hayato <hayato.314.nico@gmail.com>
2023-09-24 23:10:01 +0900

    Currencyの列挙型定義

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,9 @@
 // Enter キーを押します。これでコードに空白文字が表示されます。
 public class Main {
     public static void main(String[] args) {
-        // ハイライトされたテキストにキャレットがある状態で Opt+Enter を押して
-        // IntelliJ IDEA が提案する修正方法を表示します。
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
-        // コードを実行するには Ctrl+R を押すか、ガターにある緑の矢印ボタンをクリックします。
         for (int i = 1; i <= 5; i++) {
-
-            // Ctrl+D を押してコードのデバッグを開始します。ブレークポイントを 1 つ設定しましたが、
-            // Cmd+F8 を押すといつでも他のブレークポイントを追加できます。
             System.out.println("i = " + i);
         }
     }
