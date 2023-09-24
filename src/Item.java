5f357db8706823f24e2726830452d2e8111d7599
Hayato <hayato.314.nico@gmail.com>
2023-09-24 23:09:18 +0900

    Itemクラスの作成

---
+++ src/Item.java
@@ -0,0 +1,17 @@
+public class Item {
+    private  ItemName name;
+    private  ItemPrice price;
+
+    public Item(ItemName name, ItemPrice pricec) {
+        super();
+        if(name == null || price == null) throw  new IllegalArgumentException();
+
+        this.name = name;
+        this.price = pricec;
+    }
+
+    public ItemName getName(){
+        return name;
+    }
+
+}
